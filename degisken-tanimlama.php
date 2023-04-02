610419b37ab12b8ed474361d98e7acf22e15c838
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-04-02 18:38:58 +0300

    değişken tanımlama oluşturuldu.

---
+++ degisken-tanimlama.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $sayi1 = 10;
+    $sayi2 = 20;
+
+    $isim = "Ahmet";
+    $soyisim = 'KARAKAYA';
+
+    $dogumTarihi = "1990";
+    $dogumTarihi2 = 1990;
+
+    echo $sayi1." ".$sayi2;
+    echo "<br>";
+    echo $isim." ".$soyisim;
+    echo "<br>";
+    echo "1. Doğum Tarihi: ".$dogumTarihi;
+    echo "<br>";
+    echo "2. Doğum Tarihi: ".$dogumTarihi2;
+
+?>
