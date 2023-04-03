3ecff9c33666fe94e377eaa8099c43ef69396659
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-04-03 17:41:10 +0300

    veri tipleri oluşturuldu.

---
+++ veri-tipleri.php
@@ -0,0 +1,21 @@
+<?php 
+
+    $urunAdi = "iphone x";
+    echo gettype($urunAdi);
+
+    echo "<br>";
+
+    $fiyat = 5000;
+    echo gettype($fiyat);
+
+    echo "<br>";
+
+    $kdv = 12.3;
+    echo gettype($kdv);
+
+    echo "<br>";
+
+    $satildiMi = false;
+    echo gettype($satildiMi);
+
+?>
